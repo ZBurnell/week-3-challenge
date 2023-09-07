a6b4071f8ecd92c75d8cdfe7703a8aaceaef7a24
ZBurnell <136769106+ZBurnell@users.noreply.github.com>
2023-09-06 22:35:23 -0400

    added Var and if/else statements for char choices

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
 // Assignment Code
+var char_upper = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var char_lower = ("abcdefghijklmnopqrstuvwxyz")
+var char_num = ("0123456789")
+var char_special = ("! #$%&'()*+,-./:;<=>?@/[]^_`{|}~")
+var characters = []
 var generateBtn = document.querySelector("#generate");
+ 
 
-// Write password to the #password input
-function writePassword() {
+const length = Number(window.prompt("How long would you like your password?", "between 8 - 123"));
+
+ if (length <= 8, length >= 123){
+  alert("please enter a valid number");
+  } else (length >= 8, length <= 123)
+     var first = document.createElement[characters];
+
+var upper = confirm("Would you like to add uppercase characters to your password?");
+
+  if(upper === true){
+    var upper = document.appendChild[characters];
+  } else (upper === false)
+    var upper = null;
+
+var lower = confirm("Would you like to add lowercase characters to your password?");
+
+  if(lower === true){
+    var lower = document.appendChild[characters];
+  } else (lower === false)
+    var lower = null;
+
+var num = confirm("Would you like to add numbers to your password?");
+
+  if(num === true){
+    var num = document.appendChild[characters];
+  } else (num === false)
+    var num = null;
+
+var special = confirm("Would you like to add special characters to your password?");
+
+  if(special === true){
+    var special = document.appendChild[characters];
+  } else (special === false)
+    var special = null;
+
+ // Write password to the #password input
+
+ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  split("").characters.function(C);
+  return retval.includes(C)
+ }
+ 
   passwordText.value = password;
+  
 
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
